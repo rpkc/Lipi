8a18ece98e350eca271af9df118d30e61c2a31e6
Rupak Chowdhury <rpkc.mail@gmail.com>
2021-04-15 15:54:17 +0530

    Add files via upload

---
+++ pyavrophonetic/utils/__init__.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""Utils package for Avro Phonetic.
+
+-----------------------------------------------------------------------
+
+Copyright (C) 2013 Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in.
+
+This file is part of pyAvroPhonetic.
+
+pyAvroPhonetic is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pyAvroPhonetic is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pyAvroPhonetic.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+# Imports
+import sys
+
+
+def utf(text):
+    """Shortcut funnction for encoding given text with utf-8."""
+    if sys.version_info.major <= 2:
+        try:
+            output = unicode(text, encoding='utf-8')
+        except UnicodeDecodeError:
+            output = text
+        except TypeError:
+            output = text
+        return output
+    else:
+        return text
